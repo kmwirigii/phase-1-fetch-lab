758a3e8f02589a98757b3241321a9d6ef8b331b3
Kelvin Mwirigi <kmwirigi05@gmail.com>
2025-03-24 12:58:42 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")  // API endpoint for Game of Thrones books
+  .then(response => response.json())  // Convert response to JSON
+  .then(data => renderBooks(data));
 }
 
 function renderBooks(books) {
